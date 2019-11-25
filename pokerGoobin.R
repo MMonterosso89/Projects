8d6aee6b6132a44155423ad57c1f30cdc2b1c3c5
Matthew Monterosso <MatthewAMonterosso@gmail.com>
2019-11-25 00:59:02 -0500

    Poker Engine WIP
    added pseudo-code winner finding logic

--- pokerGoobin.R
+++ pokerGoobin.R
@@ -4,7 +4,7 @@
 
 
 library(tidyverse)
-library(merTools)
+library(merTools) # why?
 
 suits <- c("h","s","d","c")
 card <- c(2:10,"J","Q","K","A")
@@ -23,22 +23,22 @@ Deal_Players <- function(n){
 
   if (n %% 1 != 0)stop("Please Use Integer Between 1 and 9")
   if (n < 1 | n > 9)stop("Please Use Integer Between 1 and 9")
-  
-  
+
+
 assign("Deals", sample_n(deck, 2 * n, replace = FALSE), envir = globalenv())
 
-  
-    
+
+
 for (i in 1:n){
-  
-assign(paste0("P", i ), as_tibble(rbind(Deals[i,],Deals[i + n,])), envir = globalenv())  
-  
-  
-}  
+
+assign(paste0("P", i ), as_tibble(rbind(Deals[i,],Deals[i + n,])), envir = globalenv())
+
+
+}
 }
 #####
 
-Deal_Players(4)
+Deal_Players(1)
 
 
 
@@ -47,35 +47,79 @@ Deal_Players(4)
 
 #####
 Deck_Pre <- setdiff(deck, Deals)
-
+#Deck_Flop <- setdiff(Deck_Pre, Deals)
 
 
 ####Deal Logic
 Deal_Board <- function(street){
-  
-  if (street %in% c(1,2,3) = FALSE)stop("Please Choose Appropriate Street Value")  
-  
-  assign("Burn", sample_n(deck, 2 * street, replace = FALSE), envir = globalenv())
-  
-  
-  if (steet == 1){
-  assign("Flop", sample_n(deck, 3, replace = FALSE), envir = globalenv())
-  }
-  
+
+  #if (street != 1 | street != 2 | street != 3)stop("Please Choose Appropriate Street Value")
+
+  # assign("Burn", sample_n(deck, street, replace = FALSE), envir = globalenv())
+  #
+  # assign("deck", setdiff(deck, Burn))
+
+
+   if (street == 1){
+   assign("Flop", sample_n(deck, 3, replace = FALSE), envir = globalenv())
+   }
+
+   if (street == 2){
+     assign("Turn", sample_n(deck, 4, replace = FALSE), envir = globalenv())
+   }
+
+   if (street == 3){
+     assign("River", sample_n(deck, 5, replace = FALSE), envir = globalenv())
+   }
+
+
 }
 
 
+Deal_Board(street = 3)
+
+
+
+###Ideas as I make this
+
+# 1. Drought-assist - model likihood of having consistently poor hands for how long - every hand
+# indepenent event obvs but chances of not getting any playable hand diminish over sufficient sample.
+
+# 2. M O I S T - examing board texture and how common wet vs dry boards occur (and quantify what this even means), and chance for dry to become moist on T/R ("Hottness" via flopzilla)
+
+# 3. Re-calculate best hands via 1MM+ simluation with all hand combos
 #######
 
 
+
+##Start low - search toward high, wont overwrite better hands
+
+## calc if pair before search high hand?
+
+
+## All seven cards search for pair - n_distinct value or card >= 6 , must be pair,
+
+# n_distinct == 5 must be two pair (or better but find this first)
+
+# Could also do group by card , summarize n = n() for a lot of this logic
+
+# straights - hardest lemme do some thinking
+
+# group by suit if n >= 5 must be flush
+
+# quads ez
+
+# sf hmmmmm
+
+
 HandWin <- function(P1,...){
-  
+
 HandRank <- c("HighHand","OnePair","TwoPair","Trips/Set","Straight","Flush","FullHouse","Quads","StraightFlush")
-RankVal <- 1:9  
+RankVal <- 1:9
 
-Win_df <- as_tibble(cbind(RankVal,HandRank))    
-  
+Win_df <- as_tibble(cbind(RankVal,HandRank))
 
-  
-  
+
+
+
 }
